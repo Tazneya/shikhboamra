599fca06a8d31a6f303b56992a16d9f46a4c1a0f
n0k1b <nokibevon7@gmail.com>
2021-08-05 22:20:21 +0600

    Form issue solved

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	
+
 <!-- Mirrored from gambolthemes.net/html-items/cursus_demo_f12/sign_up_steps.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 11 Oct 2020 02:38:13 GMT -->
 <head>
-		<meta charset="utf-8">		
+		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, shrink-to-fit=9">
 		<meta name="description" content="Gambolthemes">
 		<meta name="author" content="Gambolthemes">
 		<title>Cursus - Sign Up Next Step</title>
-		
+
 		<!-- Favicon Icon -->
 		<link rel="icon" type="image/png" href="{{asset('sourcefile_home')}}/cursus/images//fav.png">
-		
+
 		<!-- Stylesheets -->
 		<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet'>
 		<link href="{{asset('sourcefile_home')}}/cursus/vendor/unicons-2.0.1/css/unicons.css" rel='stylesheet'>
@@ -21,15 +21,21 @@
 		<link href="{{asset('sourcefile_home')}}/cursus/css/style.css" rel="stylesheet">
 		<link href="{{asset('sourcefile_home')}}/cursus/css/responsive.css" rel="stylesheet">
 		<link href="{{asset('sourcefile_home')}}/cursus/css/night-mode.css" rel="stylesheet">
-		
+
 		<!-- Vendor Stylesheets -->
 		<link href="{{asset('sourcefile_home')}}/cursus/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
 		<link href="{{asset('sourcefile_home')}}/cursus/vendor/OwlCarousel/assets/owl.carousel.css" rel="stylesheet">
 		<link href="{{asset('sourcefile_home')}}/cursus/vendor/OwlCarousel/assets/owl.theme.default.min.css" rel="stylesheet">
 		<link href="{{asset('sourcefile_home')}}/cursus/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="{{asset('sourcefile_home')}}/cursus/stylesheet" type="text/css" href="vendor/semantic/semantic.min.css">	
-		
-	</head> 
+		<link rel="{{asset('sourcefile_home')}}/cursus/stylesheet" type="text/css" href="vendor/semantic/semantic.min.css">
+
+	</head>
+
+    <style>
+        label{
+            float: left;
+        }
+    </style>
 
 <body>
 	<!-- Signup Start -->
@@ -42,7 +48,7 @@
 						<a href="index-2.html"><img class="logo-inverse" src="{{asset('sourcefile_home')}}/cursus/images//ct_logo.svg" alt=""></a>
 					</div>
 				</div>
-			
+
 				<div class="col-lg-6 col-md-8">
 					<div class="sign_form">
 						<div class="main-tabs">
@@ -52,45 +58,39 @@
 								</li>
 								<li class="nav-item">
 									<a href="#student-signup-tab" id="student-tab" class="nav-link" data-toggle="tab">Student Sign Up</a>
-								</li>																				
-							</ul>									
+								</li>
+							</ul>
 						</div>
 						<div class="tab-content" id="myTabContent">
 							<div class="tab-pane fade show active" id="instructor-signup-tab" role="tabpanel" aria-labelledby="instructor-tab">
 								<p>Sign Up and Create Course!</p>
 								<form>
-								
-									<select class="ui hj145 dropdown cntry152 prompt srch_explore">
-										<option value="">Select Category</option>
-										<option value="1">Development</option>
-										<option value="2">Business</option>
-										<option value="3">Finance & Accounting</option>
-										<option value="4">IT & Software</option>
-										<option value="5">Office Productivity</option>
-										<option value="6">Personal Development</option>
-										<option value="7">Design</option>
-										<option value="8">Marketing</option>
-										<option value="9">Lifestyle</option>
-										<option value="10">Photography</option>
-										<option value="11">Health & Fitness</option>
-										<option value="12">Music</option>
-										<option value="13">Teaching & Academics</option>
-									</select>
-									<div class="ui search focus mt-15">																
-										<div class="ui form swdh30">
-											<div class="field">
-												<textarea rows="3" name="description" id="id_about" placeholder="Write a little description about you..."></textarea>
-											</div>
-										</div>
-										<div class="help-block">Your biography should have at least 12000 characters.</div>
-									</div>							
-									<button class="login-btn" type="submit">Instructor Sign Up Now</button>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlInput1">Email address</label>
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Example select</label>
+                                        <select class="form-control" id="exampleFormControlSelect1">
+                                          <option>1</option>
+                                          <option>2</option>
+                                          <option>3</option>
+                                          <option>4</option>
+                                          <option>5</option>
+                                        </select>
+                                      </div>
+
+                                      <div class="form-group">
+                                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                      </div>
+
 								</form>
 							</div>
 							<div class="tab-pane fade" id="student-signup-tab" role="tabpanel" aria-labelledby="student-tab">
 								<p>Sign Up and Start Learning!</p>
 								<form>
-									<div class="ui search focus mt-15">																
+									<div class="ui search focus mt-15">
 										<div class="ui form swdh30">
 											<div class="field">
 												<textarea rows="3" name="description" id="id_about1" placeholder="Write a little description about you..."></textarea>
@@ -105,19 +105,19 @@
 						<p class="mb-0 mt-30">Already have an account? <a href="sign_in.html">Log In</a></p>
 					</div>
 					<div class="sign_footer"><img src="{{asset('sourcefile_home')}}/cursus/images//sign_logo.png" alt="">© 2020 <strong>Cursus</strong>. All Rights Reserved.</div>
-				</div>				
-			</div>				
-		</div>				
+				</div>
+			</div>
+		</div>
 	</div>
-	<!-- Signup End -->	
+	<!-- Signup End -->
 
 	<script src="{{asset('sourcefile_home')}}/cursus/js/jquery-3.3.1.min.js"></script>
 	<script src="{{asset('sourcefile_home')}}/cursus/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 	<script src="{{asset('sourcefile_home')}}/cursus/vendor/OwlCarousel/owl.carousel.js"></script>
 	<script src="{{asset('sourcefile_home')}}/cursus/vendor/semantic/semantic.min.js"></script>
-	<script src="{{asset('sourcefile_home')}}/cursus/js/custom.js?{{time()}}"></script>	
-	<script src="{{asset('sourcefile_home')}}/cursus/js/night-mode.js"></script>	
-	
+	<script src="{{asset('sourcefile_home')}}/cursus/js/custom.js?{{time()}}"></script>
+	<script src="{{asset('sourcefile_home')}}/cursus/js/night-mode.js"></script>
+
 </body>
 
 <!-- Mirrored from gambolthemes.net/html-items/cursus_demo_f12/sign_up_steps.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 11 Oct 2020 02:38:13 GMT -->
